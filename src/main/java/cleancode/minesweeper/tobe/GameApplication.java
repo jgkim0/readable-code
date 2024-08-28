3015140b207f937e065b9703f59fec7e1bd29046
jgkim0 <jgkimm9@gmail.com>
2024-08-28 23:51:26 +0900

    섹션4-5. 단일 책임의 원칙 SRP

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -9,6 +9,8 @@ public class GameApplication {
     public static void main(String[] args){
         Minesweeper minesweeper = new Minesweeper();
         minesweeper.run();
+
+        // 메인 분리
     }
 
 
